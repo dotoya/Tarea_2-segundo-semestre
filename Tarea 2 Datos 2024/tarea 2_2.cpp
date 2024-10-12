52a32a00a053f9ba7c90ee5973ad6059fa4d3136
SMR0411 <145221247+SMR0411@users.noreply.github.com>
2024-10-12 20:49:22 -0300

    Add files via upload

--- Tarea 2 Datos 2024/tarea 2_2.cpp
+++ Tarea 2 Datos 2024/tarea 2_2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 struct Pelicula {
@@ -23,7 +24,9 @@ private :
     string nombre_director ;
 public:
 
-    void agregar_pelicula ( Pelicula* pelicula ) ; // agrega pelicula al final de la lista enlazada
+    void agregar_pelicula ( Pelicula* pelicula ){
+
+    } ; // agrega pelicula al final de la lista enlazada
     void ordenar () ; // ordena la lista
     void calcular_rating_promedio () ;
     void mostrar_peliculas () ;
@@ -47,10 +50,77 @@ public :
 
     Arboles () ; // constructor
     ~ Arboles () ; // destructor
-    void insertar_pelicula (Pelicula* pelicula ) ;
+    void insertar_pelicula (Pelicula* pelicula ){
+        
+    } ;
     void copiar_arbol (); // hace copia de arbol 1 en arbol 2 ordenado respecto de rating
     Director* buscar_director ( string director ) ; // retorna arreglo de peliculas
     Pelicula* buscar_pelicula ( string pelicula ) ; // retorna peliculas 3
     void mejores_directores ( int n ) ; // Muestra por pantalla los mejores n directores .Enumerando de 1 a n .
     void peores_directores ( int n ) ; // Muestra por pantalla los peores n directores .Enumerando desde m ( cantidad de directores ) hasta m - n .
+};
+
+int main(){
+    ifstream file;
+
+    file.open("Peliculas.txt");
+
+    if (!file.is_open()){
+    cout<<"error al abrir el archivo"<<endl;
+    exit(1);}
+    int t_p=0;
+    float *rat;
+    string *l_p, *pel, *dir;
+    string ph, pa; 
+
+    for(int i=0;i<1;i++){
+        getline(file, ph);
+        t_p= stoi(ph);
+    };
+    cout<<t_p<<endl;
+
+    l_p = new string[t_p];
+    pel = new string[t_p];
+    dir = new string[t_p];
+    rat = new float[t_p];
+    for(int z= 0; z<(t_p);z++){
+        getline(file, ph);
+        l_p[z] = ph;
+        l_p[z] += ' ';
+    }
+    cout<<l_p[0]<<endl;
+    cout<<l_p[1]<<endl;
+    cout<<l_p[2]<<endl;
+    for(int c= 0; c<(t_p);c++){
+        int posac= 0;
+        pa= "";
+        for(l_p[c][posac]; l_p[c][posac] != ';'; posac++){
+            pel[c]+= l_p[c][posac];
+        }
+        posac+=1;
+        for(l_p[c][posac]; l_p[c][posac] != ';'; posac++){
+            dir[c]+= l_p[c][posac];
+        }
+        posac+=1;
+        for(l_p[c][posac]; l_p[c][posac] != ' '; posac++){
+            pa+= l_p[c][posac];
+        }
+        rat[c]= stof(pa);
+    }
+    cout<<pel[0]<<endl;
+    cout<<pel[1]<<endl;
+    cout<<pel[2]<<endl;
+    cout<<dir[0]<<endl;
+    cout<<dir[1]<<endl;
+    cout<<dir[2]<<endl;
+    cout<<rat[0]<<endl;
+    cout<<rat[1]<<endl;
+    cout<<rat[2]<<endl;
+    Pelicula *con;
+    con= new Pelicula[t_p];
+    for(int u= 0; u<(t_p);u++){
+        con[u].nombre= pel[u];
+        con[u].director= dir[u];
+        con[u].rating= rat[u];
+    }
 };
